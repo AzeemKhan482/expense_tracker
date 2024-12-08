2ef97855d0adec68e92b9b11b033580e1cdff133
Azeem Khan <104724412+AzeemKhan482@users.noreply.github.com>
2024-12-08 13:50:50 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-home: const Expenses(),
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.onSecondary,
+      ),
+      cardTheme:const CardTheme().copyWith(
+        //color: kColorScheme.onPrimaryFixedVariant,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      )
+      
+    ), //its better to change color from start better to use .copyWith() and apply to some parts 
+    home: const Expenses(),
   ));
 }
